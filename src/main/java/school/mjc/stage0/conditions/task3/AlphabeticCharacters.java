3285b89cf77ec65a6f3fcc404429d75aedd74120
Mikalai Alkhimenka <alkhimenkamikalai90@gmail.com>
2023-07-24 22:34:27 +0200

    Solved

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+            if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' || character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double tax = 0.15;
+            double salaryAfterTax = salary - (salary * tax);
+            System.out.println(salaryAfterTax);
+        } else if (salary <= 20000) {
+            double tax = 0.18;
+            double salaryAfterTax = salary - (salary * tax);
+            System.out.println(salaryAfterTax);
+        } else {
+            double tax = 0.2;
+            double salaryAfterTax = salary - (salary * tax);
+            System.out.println(salaryAfterTax);
     }
 }
+}
+
